b15461c751d854d2717c474c6d0a64bf5ff2e8be
Kjell Kinne <122307328+MagicKey08@users.noreply.github.com>
2024-05-25 22:58:27 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function hide() {		//Das ist die Funktion damit das Cookie popup clickbar ist
-  var x = document.getElementById("simple-cookie-consent");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
